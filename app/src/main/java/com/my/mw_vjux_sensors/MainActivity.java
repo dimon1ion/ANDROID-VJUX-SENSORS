7375d59b50d79b7a6f8c980bdc3beaebef901148
dimon1ion <dmitry-ben10@mail.ru>
2023-05-14 23:58:10 +0400

    Initial commit

---
+++ app/src/main/java/com/my/mw_vjux_sensors/MainActivity.java
@@ -0,0 +1,14 @@
+package com.my.mw_vjux_sensors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
